822dfede308a227a1f6c76d70cdc108424f6639b
Oz N Tiram <oz.tiram@gmail.com>
2018-01-11 15:40:57 +0100

    add option to install extra packages

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,14 @@ PYPI_PORT=${PYPI_PORT:-80}
 PYPI_PASSWD_FILE="${PYPI_PASSWD_FILE:-${PYPI_ROOT}/.htpasswd}"
 PYPI_AUTHENTICATE="${PYPI_AUTHENTICATE:-update}"
 
+APK_ADD="${APK_ADD}"
+
+# use this to install extra packages
+# e.g. py-gunicorn
+if [ ! -z $APK_ADD ]; then
+    apk add ${APK_ADD}
+fi 
+
 # make sure the passwd file exists
 touch "${PYPI_PASSWD_FILE}"
 
