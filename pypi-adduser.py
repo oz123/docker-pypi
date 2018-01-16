c39909e12f7a832659190bdd5a7f12e40c835bca
Oz N Tiram <oz.tiram@mobilityhouse.com>
2018-01-16 12:01:57 +0100

    Fix logic and hard coded path in pypi-adduser

--- pypi-adduser.py
+++ pypi-adduser.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python
+
 import os
 import getpass
 import sys
+
+from passlib.apache import HtpasswdFile
 
 if len(sys.argv) < 2:
     print("usage: {} FILENAME".format(sys.argv[0]))
     exit(1)
 
-from passlib.apache import HtpasswdFile
+secrets_file = sys.argv[1]
 
-secrets = HtpasswdFile(secrets_file, new=os.path.exists(secrets_file))
+if os.path.exists(secrets_file):
+    try:
+        append = True if sys.argv[2] == '-a' else False
+    except IndexError:
+        append = False
+else:
+    with open(secrets_file, mode='w') as f:
+        append = False
+
+secrets = HtpasswdFile(sys.argv[1], new=append)
 
 username = raw_input("User:")
 password = getpass.getpass()
