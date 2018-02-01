7c7c50aeec144dce0c8ce12b13c58378bdc201ed
Oz N Tiram <oz.tiram@gmail.com>
2018-02-01 09:59:32 +0100

    Remove HERE document to build of image

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,19 @@
 FROM alpine:3.5
 MAINTAINER  Oz Tiram <oz123@gmail.com>
 
-
 RUN apk update && \
     apk add py-pip uwsgi uwsgi-python && \
-    pip install --upgrade pip
+    pip install --upgrade pip && \
+    mkdir /var/run
 
 RUN pip install -U passlib pypiserver==1.2.1 watchdog
+# patch the file from pypi server
+# see https://github.com/pypiserver/pypiserver/issues/201
+RUN sed '257 a \        sys.argv.remove(k) if k in sys.argv else 1\n' -i /usr/lib/python2.7/site-packages/pypiserver/__main__.py
 
 VOLUME ["/srv/pypi"]
+
+WORKDIR /var/run/
 
 ADD entrypoint.sh /usr/bin/
 ADD pypi-adduser.py /usr/bin/pypi-adduser.py
--- entrypoint.sh
+++ entrypoint.sh
@@ -7,23 +7,7 @@ PYPI_AUTHENTICATE="${PYPI_AUTHENTICATE:-update}"
 UWSGI_PROCS=${UWSGI_PROCS:-4}
 UWSGI_THREADS=${UWSGI_THREADS:-2}
 
-_extra="${PYPI_EXTRA}"
-
-# patch the file from pypi server
-sed '257 a \        sys.argv.remove(k) if k in sys.argv else 1\n' -i /usr/lib/python2.7/site-packages/pypiserver/__main__.py
-
-
-cat > /usr/lib/python2.7/site-packages/pypiserver/uwsgi.py <<EOF
-from pypiserver import app
-
-wsgi_app = app(
-    root='${PYPI_ROOT}',
-    verbosity=2,
-    port=${PYPI_PORT},
-    authenticated=['update', 'list', 'download'],
-    password_file='${PYPI_PASSWD_FILE}'
-)
-EOF
-fi
-
-exec uwsgi --need-app --plugins-dir /usr/lib/uwsgi --plugin python --processes ${UWSGI_PROCS} --threads ${UWSGI_THREADS} --http-socket :${PYPI_PORT} --wsgi-file=/usr/lib/python2.7/site-packages/pypiserver/uwsgi.py --callable=wsgi_app --master
+exec uwsgi --need-app --plugins-dir /usr/lib/uwsgi \
+	--plugin python --processes ${UWSGI_PROCS} \
+	--threads ${UWSGI_THREADS} --http-socket :${UWSGI_PYPI_PORT} \
+	--wsgi-file=pypi.wsgi --callable=wsgi_app --master
