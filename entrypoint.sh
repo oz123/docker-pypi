a1977ed8cc377a35c19a57b85a65f572ea966dc7
Oz N Tiram <oz.tiram@gmail.com>
2018-02-01 11:11:47 +0100

    Correct port variable name

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-UWSGI="${UWSGI_PYPI_PORT:-9001}"
+UWSGI_PORT="${UWSGI_PORT:-9001}"
 UWSGI_PROCS=${UWSGI_PROCS:-4}
 UWSGI_THREADS=${UWSGI_THREADS:-2}
 
 exec uwsgi --need-app --plugins-dir /usr/lib/uwsgi \
 	--plugin python --processes ${UWSGI_PROCS} \
-	--threads ${UWSGI_THREADS} --http-socket :${UWSGI_PYPI_PORT} \
+	--threads ${UWSGI_THREADS} --http-socket 0.0.0.0:${UWSGI_PORT} \
 	--wsgi-file=pypi.wsgi --callable=wsgi_app --master
