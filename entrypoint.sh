c9c58a573de6a85176968ab2814e281a2b44800f
Greg Lutostanski <greg.lutostanski@mobilityhouse.com>
2018-02-06 17:05:25 +0100

    fix typo, works either way but --lazy-apps is the correct flag

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,4 @@ UWSGI_THREADS=${UWSGI_THREADS:-2}
 exec uwsgi --need-app --plugins-dir /usr/lib/uwsgi \
 	--plugin python --processes ${UWSGI_PROCS} \
 	--threads ${UWSGI_THREADS} --http-socket 0.0.0.0:${UWSGI_PORT} \
-	--wsgi-file=pypi.wsgi --callable=wsgi_app --master --lazy-app
+	--wsgi-file=pypi.wsgi --callable=wsgi_app --master --lazy-apps
